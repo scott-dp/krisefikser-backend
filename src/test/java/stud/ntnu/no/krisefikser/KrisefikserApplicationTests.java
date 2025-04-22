7af3d58682135a5f2fef4fa75d756dedc1a358a0
jonathan <jonathhu@stud.ntnu.no>
2025-04-22 11:37:00 +0200

    Initialize spring boot project

---
+++ src/test/java/stud/ntnu/no/krisefikser/KrisefikserApplicationTests.java
@@ -0,0 +1,13 @@
+package stud.ntnu.no.krisefikser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KrisefikserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
