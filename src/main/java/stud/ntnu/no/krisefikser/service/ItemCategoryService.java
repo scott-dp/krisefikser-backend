4b3e4dee759074f8929d5f9a17454285d1800b38
Sander Sandvik Nessa <sandesn@stud.ntnu.no>
2025-04-24 10:39:54 +0200

    Add logging to ItemCategoryService

--- src/main/java/stud/ntnu/no/krisefikser/service/ItemCategoryService.java
+++ src/main/java/stud/ntnu/no/krisefikser/service/ItemCategoryService.java
@@ -1,6 +1,8 @@
 package stud.ntnu.no.krisefikser.service;
 
 import lombok.RequiredArgsConstructor;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Service;
 
 import stud.ntnu.no.krisefikser.dtos.itemCategory.ItemCategoryResponse;
@@ -10,18 +12,20 @@ import stud.ntnu.no.krisefikser.repository.ItemCategoryRepository;
 import java.util.List;
 import java.util.stream.Collectors;
 
-//TODO: Add tests and logging
+//TODO: Add tests
 
 /**
  * Service class for managing ItemCategory entities.
  * <p>
- * This class provides a method to retrieve all item categories and convert them to DTOs.
+ * This class provides a method to retrieve all item categories and convert them
+ * to DTOs.
  * </p>
  */
 @Service
 @RequiredArgsConstructor
 public class ItemCategoryService {
-  
+
+  private static final Logger logger = LogManager.getLogger(ItemCategoryService.class);
   private final ItemCategoryRepository categoryRepository;
 
   /**
@@ -30,8 +34,11 @@ public class ItemCategoryService {
    * @return a list of {@link ItemCategoryResponse} DTOs representing all item categories
    */
   public List<ItemCategoryResponse> getAllItemCategories() {
-    return categoryRepository.findAll().stream()
+    logger.info("Fetching all item categories from the database");
+    List<ItemCategoryResponse> categories = categoryRepository.findAll().stream()
         .map(ItemCategoryMapper::toDto)
         .collect(Collectors.toList());
+    logger.info("Retrieved {} item categories", categories.size());
+    return categories;
   }
 }
