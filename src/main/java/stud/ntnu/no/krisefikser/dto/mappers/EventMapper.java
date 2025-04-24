bd63509ee3f4238a1f953faeafb0ae6f489e3aaf
aryma <arymalekian@gmail.com>
2025-04-24 13:36:48 +0200

    Update EventMapper with proper functionality for mapping to dto

--- src/main/java/stud/ntnu/no/krisefikser/dto/mappers/EventMapper.java
+++ src/main/java/stud/ntnu/no/krisefikser/dto/mappers/EventMapper.java
@@ -6,10 +6,13 @@ import org.apache.logging.log4j.Logger;
 import org.locationtech.jts.geom.Geometry;
 import org.locationtech.jts.io.geojson.GeoJsonReader;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import stud.ntnu.no.krisefikser.entities.Event;
 import stud.ntnu.no.krisefikser.dto.EventRequest;
 import stud.ntnu.no.krisefikser.dto.EventResponse;
 import stud.ntnu.no.krisefikser.entities.EventType;
+import stud.ntnu.no.krisefikser.exception.CustomErrorMessage;
+import stud.ntnu.no.krisefikser.exception.customExceptions.InvalidGeoJsonException;
 
 /**
  * Service for mapping between {@link Event} entities and their corresponding DTOs.
@@ -24,29 +27,32 @@ public class EventMapper {
 
   private static final Logger logger = LogManager.getLogger(EventMapper.class);
 
-
-
-
   /**
    * Converts a {@link EventRequest} DTO to a {@link Event} entity.
    *
    * @param request the {@link EventRequest} DTO containing the event data
+   * @param eventType the {@link EventType} associated with the event
    * @return the corresponding {@link Event} entity
+   * @throws InvalidGeoJsonException if the GeoJSON string is invalid
    */
-  public Event toEntity(EventRequest dto, EventType eventType) {
+  public Event toEntity(EventRequest request, EventType eventType) {
+    logger.debug("Mapping EventRequest to Event entity for event type '{}'",
+        () -> eventType.getName() + " (ID: " + eventType.getId() + ")");
+
     Event event = new Event()
-        .setTitle(dto.getTitle())
-        .setDescription(dto.getDescription())
-        .setSeverity(dto.getSeverity())
+        .setTitle(request.getTitle())
+        .setDescription(request.getDescription())
+        .setSeverity(request.getSeverity())
         .setType(eventType);
 
     try {
       GeoJsonReader reader = new GeoJsonReader();
-      Geometry geometry = reader.read(dto.getGeometryGeoJson());
+      Geometry geometry = reader.read(request.getGeometryGeoJson());
       geometry.setSRID(4326);
       event.setGeometry(geometry);
     } catch (Exception e) {
-      throw new RuntimeException("Invalid GeoJSON input", e);
+      logger.error("Invalid GeoJSON format: {}", request.getGeometryGeoJson(), e);
+      throw new InvalidGeoJsonException(CustomErrorMessage.GEOJSON_NOT_VALID);
     }
 
     logger.info("Mapped EventRequest to Event entity: {}", event.getId());
