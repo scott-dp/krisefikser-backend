81ebe0c1d81414a7166277f75c26616f01520274
aryma <arymalekian@gmail.com>
2025-04-24 11:05:55 +0200

    Create EventTypeResponse DTO

---
+++ src/main/java/stud/ntnu/no/krisefikser/dto/EventTypeResponse.java
@@ -0,0 +1,25 @@
+package stud.ntnu.no.krisefikser.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Accessors(chain = true)
+@Schema(description = "DTO for representing an EventType in API responses. " +
+    "Contains name and description.")
+public class EventTypeResponse {
+
+  @Schema(description = "Name of the event type", example = "Natural Disaster")
+  private String name;
+
+  @Schema(description = "Description of the event type", example = "Covers natural hazards like " +
+      "earthquakes or floods.")
+  private String description;
+}
