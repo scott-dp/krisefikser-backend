5cb6d17df13fad7b726f18909071a8e2d460bdd0
Scott <scottld@stud.ntnu.no>
2025-04-24 12:23:48 +0200

    Hotfix for changed package names

--- src/main/java/stud/ntnu/no/krisefikser/dtos/event/EventTypeRequest.java
+++ src/main/java/stud/ntnu/no/krisefikser/dtos/event/EventTypeRequest.java
@@ -1,4 +1,4 @@
-package stud.ntnu.no.krisefikser.dto;
+package stud.ntnu.no.krisefikser.dtos.event;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -6,19 +6,21 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.Accessors;
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
 
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @Accessors(chain = true)
-@Schema(description = "DTO for representing an EventType in API responses. " +
-    "Contains name and description.")
-public class EventTypeResponse {
+@Schema(description = "DTO for creating or updating an EventType. Contains name, and description.")
+public class EventTypeRequest {
 
+  @NotBlank(message = "Name must not be blank")
   @Schema(description = "Name of the event type", example = "Natural Disaster")
   private String name;
 
+  @NotBlank(message = "Description must not be blank")
   @Schema(description = "Description of the event type", example = "Covers natural hazards like " +
       "earthquakes or floods.")
   private String description;
